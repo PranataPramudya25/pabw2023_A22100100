fc70db9edf45d948bf4cb6d0784351701ee45289
Pranata Pramudya <a22100100@mhs.stmik-sumedang.ac.id>
2023-04-22 23:51:59 +0700

    menambahkan folder pertemuan8

---
+++ kuliah/pertemuan8/latihan1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nrp</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <td>1</td>
+    <td><img src="img/Pranata.jpeg" width="60"></td>
+    <td>A2.2100100</td>
+    <td>Pranata Pramudya</td>
+    <td>pranajaya52@gmail.com</td>
+    <td>Teknik Informatika</td>
+    <td>
+      <a href="">ubah</a> | <a href="">hapus</a>
+    </td>
+
+  </table>
+</body>
+
+</html>
