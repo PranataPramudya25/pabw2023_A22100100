52ad5979ed1759013345412c3698b0382b5ed353
Pranata Pramudya <a22100100@mhs.stmik-sumedang.ac.id>
2023-04-23 17:16:00 +0700

    koneksi dan tampil data

--- kuliah/pertemuan8/latihan2.php
+++ kuliah/pertemuan8/latihan2.php
@@ -1,21 +1,6 @@
 <?php
-//koneksi ke DB & pilih database
-$conn = mysqli_connect('localhost', 'root', '', 'pw_a22100100');
-
-// quey tabel isi mahasiswa
-$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
-
-// ubah data ke dalam array
-//$row = mysqli_fetch_row($result); //array numerik
-//$row = mysqli_fetch_assoc($result); //array associative
-//$row = mysqli_fetch_array($result); // keduanya
-$rows = [];
-while ($row = mysqli_fetch_assoc($result)) {
-  $rows[] = $row;
-}
-
-//tampung ke variabel mahasiswa
-$mahasiswa = $rows;
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
 ?>
 
 
